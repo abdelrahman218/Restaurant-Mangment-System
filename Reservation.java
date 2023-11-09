7acfde50c60587c6aa2dc27af5ea91c3fbb2def0
abdelrahman218 <144768458+abdelrahman218@users.noreply.github.com>
2023-11-09 19:36:51 +0200

    Add files via upload

--- Reservation.java
+++ Reservation.java
@@ -7,7 +7,7 @@ public class Reservation {
     private int numOfGuests;
     private double price;
     private byte rating;
-    private Date Date;
+    private Date date;
     private LocalTime startTime;
     private LocalTime endTime;
     public Reservation(int reservationNum,int tableNum,int numOfGuests,double price,Date Date,LocalTime startTime,LocalTime endTime){
@@ -15,8 +15,15 @@ public class Reservation {
         this.tableNum=tableNum;
         this.numOfGuests=numOfGuests;
         this.price=price;
-        this.Date= Date;
+        this.date= Date;
         this.startTime=startTime;
         this.endTime=endTime;
+    }
+    public int getReservationNumber(){return reservationNum;}
+    public Date getDate(){return date;}
+    @Override
+    public String toString(){
+        return("Reservation No.: "+reservationNum+", Table No.: "+tableNum+", No. of Guests: "+numOfGuests+
+        ", Cost: "+price+", Date: "+date+", From: "+startTime+" To: "+endTime+", Guests rating: "+rating);
     }
 }
