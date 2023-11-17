62f3a347f9ff5576ffdbdaab02ac882cb73e3ff2
Omar400-cpu <148485672+Omar400-cpu@users.noreply.github.com>
2023-11-17 16:18:43 +0200

    Update Meal.java

--- Meal.java
+++ Meal.java
@@ -1,24 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package System;
 
-/**
- *
- * @author Omar
- */
-
+import java.util.ArrayList;
 public class Meal {
 
+   private static ArrayList<Meal> mealList = new ArrayList<>();
+    private String meal_ID;
     private String Name;
     private double Price;
     private String type;
     private int noOfOrders;
 
-    public Meal(String Name, double Price, String type) {
+    public Meal(String menu_ID, String meal_ID, String Name, double Price, String type, int noOfOrders) {
+        this.meal_ID = meal_ID;
         this.Name = Name;
         this.Price = Price;
         this.type = type;
+        this.noOfOrders = noOfOrders;
+    }
+    public String getMeal_ID() {
+        return meal_ID;
     }
 
     protected String getName() {
@@ -39,6 +39,20 @@ public class Meal {
 
     public void incrementedOrders() {
         noOfOrders++;
+    }
+
+    public static ArrayList<Meal> getMealList() {
+        return mealList;
+    }
+
+    public static Meal getMealById(String id) {
+        for (int i = 0; i < mealList.size(); i++) {
+            Meal meal = mealList.get(i);
+            if (meal.getMeal_ID().equals(id)) {
+                return meal;
+            }
+        }
+        return null; 
     }
 }
 
