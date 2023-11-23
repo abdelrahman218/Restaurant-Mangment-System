e4e7de88e03e5f12435ec97bdb01459db6e5609d
Ryderclaw <120842081+Ryderclaw@users.noreply.github.com>
2023-11-23 13:13:01 +0200

    Update PersonClass.java

--- PersonClass.java
+++ PersonClass.java
@@ -1,26 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package person;
+package javaapplication3;
 
-/**
- *
- * @author Admin
- */
-public class PersonClass {
+public abstract class PersonClass {
+    private int ID;
+    private static int idGenerator=0;
     private String Name;
+    private String UserName;
+    private String Password;
     private String Address;
     private String DateOfBirth;
-    private int PhoneNum;
+    private String PhoneNum;
     private String Email;
 
-    public PersonClass(String Name, String Address, String DateOfBirth, int PhoneNum, String Email) {
+    public PersonClass(String Name, String Address, String DateOfBirth, String PhoneNum, String Email,String UserName,String Password) {
         this.Name = Name;
         this.Address = Address;
         this.DateOfBirth = DateOfBirth;
         this.PhoneNum = PhoneNum;
         this.Email = Email;
+        this.UserName=UserName;
+        this.Password=Password;
+        ID=++idGenerator;
     }
 
     public void setName(String Name) {
@@ -35,12 +34,20 @@ public class PersonClass {
         this.DateOfBirth = DateOfBirth;
     }
 
-    public void setPhoneNum(int PhoneNum) {
+    public void setPhoneNum(String PhoneNum) {
         this.PhoneNum = PhoneNum;
     }
 
     public void setEmail(String Email) {
         this.Email = Email;
+    }
+
+    public void setUserName(String UserName) {
+        this.UserName = UserName;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
     }
 
     public String getName() {
@@ -55,12 +62,49 @@ public class PersonClass {
         return DateOfBirth;
     }
 
-    public int getPhoneNum() {
+    public String getPhoneNum() {
         return PhoneNum;
     }
 
     public String getEmail() {
         return Email;
     }
-    
+    public int getId(){
+        return ID;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+    public boolean checkpassword(String password){
+    String encp="";
+    int j=2;
+    for(int i=0;i<password.length();i++){
+    encp+=(char)(password.charAt(i)+j);
+    }
+    if(Password==password){
+    return true;
+    }else{
+    return false;
+    }
+    }
+    public boolean checkUserName(String userName){
+    if(UserName==userName){
+    return true;
+    }else{
+    return false;
+    }
+    }
+    public void encreptPassword(){
+    String encp="";
+    int j=2;
+    for(int i=0;i<Password.length();i++){
+    encp+=(char)(Password.charAt(i)+j);
+    }
+    Password=encp;
+    }
 }
