d4b46a65f360558830d4de69ddbeb383807812e2
Yousef Ali <132370936+YousefAli-1@users.noreply.github.com>
2023-11-23 08:28:01 +0200

    Add files via upload

--- Table.java
+++ Table.java
@@ -1,25 +1,32 @@
 package System;
 import java.util.ArrayList;
 import java.util.Date;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.time.LocalTime;
-public class Table {
+public class Table implements Serializable {
     private static ArrayList<Table> Tables;
-    private ArrayList<Reservation> reservations;
+    private static int idGenerator=1;
+    private int tableNum;
     private double Revenue;
     private Category Categ;
     private int NoOfSeats;
     private double Cost;
     public Table(int NoOfSeats){
-        reservations=null;
         Revenue =0;
+        tableNum=idGenerator++;
         Cost = 100;
         this.NoOfSeats=NoOfSeats;
         this.Categ=Category.Standard;
     }
-    public static void AddReservation(Reservation data,int tableNum){
-       Tables.get(tableNum).reservations.add(data);
-       
-    }
+    public static Table getTable(int tableNum){return Tables.get(tableNum);}
+    public int getNoOfSeats(){return NoOfSeats;}
+    public double getCost(){return Cost;}
+    public Category getcateg(){return Categ;}
     public static Table mostReservedTable(Date StartDate, Date EndDate){
         int max=0;
         for(int i=1;i<Tables.size();i++){
@@ -40,39 +47,61 @@ public class Table {
         return Tables.get(max);
     }
     public boolean isReserved(Date day,LocalTime start ,LocalTime end){
+        ArrayList<Reservation> reservations=getReservations();
        for(int i=0;i<reservations.size();i++){
              if((reservations.get(i).getDate()==day)&&
                 ((start.isBefore(reservations.get(i).getEndTime())&& (start.isAfter(reservations.get(i).getStartTime())))
-                ||(end.isAfter(reservations.get(i).getStartTime())&& end.isBefore(reservations.get(i).getStartTime()))
-                ||start.equals(reservations.get(i).getStartTime())
-                ||end.equals(reservations.get(i).getEndTime())))
+                 ||(end.isAfter(reservations.get(i).getStartTime())&& end.isBefore(reservations.get(i).getStartTime()))
+                 ||start.equals(reservations.get(i).getStartTime())
+                 ||end.equals(reservations.get(i).getEndTime())))
              return true;
         }
         return false;
     }
     public void addToRevenue(){
-        Revenue+=Cost;
+        ArrayList<Reservation> reservations=getReservations();
+         for(int i=0;i<reservations.size();i++){
+             this.Revenue+=reservations.get(i).getPrice();
+        }
     } 
-    public static Table getTable(int tableNum){
-        return Tables.get(tableNum);
-    }
-    public int getNoOfSeats(){
-    return NoOfSeats;
-    }
-    public double getCost(){
-        return Cost;
-    } 
-    public Category getcateg(){
-        return Categ;
-    }
-    //New function to know the number of reservations from time to time
+    
     private int ReservationsInSpecificTime(Date StartDate, Date EndDate){
         int count=0;
+        ArrayList<Reservation> reservations=getReservations();
         for(int i=0;i<reservations.size();i++)
-            if(StartDate.before(reservations.get(i).getDate())||EndDate.after(reservations.get(i).getDate())||
-            StartDate.equals(reservations.get(i).getDate())||EndDate.equals(reservations.get(i).getDate())) 
+            if(StartDate.before(reservations.get(i).getDate())||EndDate.after(reservations.get(i).getDate())
+             ||StartDate.equals(reservations.get(i).getDate())||EndDate.equals(reservations.get(i).getDate())) 
                 count++;
         return count;
     }
-
+    // check update with Abdo
+    private ArrayList<Reservation> getReservations(){
+        ArrayList<Reservation> reservations=new ArrayList<>();
+        for(int i=0;i<Reservation.getReservations().size();i++){
+            if(Reservation.getReservations().get(i).getTableNumber()==tableNum){
+                reservations.add(Reservation.getReservations().get(i));
+            }
+        }
+        return reservations;
+    }
+    public static void saveInFile(){
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("TablesData.dat"));
+                out.writeObject(Table.Tables);
+            out.close();
+            } catch (IOException e) {
+            System.out.println(e);
+            }
+    }
+    public static void readFromFile(){
+        try {
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream("TablesData.dat"));
+            Table.Tables=(ArrayList<Table>)in.readObject();
+            in.close();
+        }catch (ClassNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
