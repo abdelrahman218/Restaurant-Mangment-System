689e73a9b24828c86ff59cbf1309665d0139aea2
Omar400-cpu <148485672+Omar400-cpu@users.noreply.github.com>
2023-11-23 12:02:18 +0200

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,12 +1,19 @@
 package System;
 import java.util.Date;
 import java.util.ArrayList;
-
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.Serializable;
 /**
  *
  * @author Omar
  */
-public class Menu {
+public class Menu implements Serializable {
 
     private static ArrayList<Menu> Menues = new ArrayList<>(50);
     ArrayList<Meal> Meals = new ArrayList<>(50);
@@ -50,5 +57,29 @@ public class Menu {
             }
         }
         return mealsInMenu;
+    }
+  public void ReadFromMenuFile() {
+        try {
+            FileInputStream i = new FileInputStream("Menu.dat");
+            ObjectInputStream o = new ObjectInputStream(i);
+            Menues = (ArrayList<Menu>) o.readObject();
+            i.close();
+            o.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }catch(ClassNotFoundException e){
+        System.out.println(e);
+        }
+    }
+  public void WriteInMenuFile() {
+        try {
+            FileOutputStream i = new FileOutputStream("Menu.dat");
+            ObjectOutputStream o = new ObjectOutputStream(i);
+            o.writeObject(Menues);
+            o.close();
+            i.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
