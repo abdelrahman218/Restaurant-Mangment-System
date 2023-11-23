ebb423757e7418bcfce7b0cc8241f2ae25eb105c
abdelrahman218 <144768458+abdelrahman218@users.noreply.github.com>
2023-11-23 16:26:10 +0200

    Update Reservation.java

--- Reservation.java
+++ Reservation.java
@@ -1,235 +1,153 @@
-package javaapplication3;
-import java.util.Scanner;
+package system;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-
 //Data Structures
 import java.util.ArrayList;
 import java.util.Date;
 import java.time.LocalTime;
-
 //Self-Defined Classes
-import javaapplication3.Reservation;
-import javaapplication3.Table;
-import javaapplication3.Meal;
-import javaapplication3.Guest;
-import javaapplication3.Receptionist;
-
+import system.Reservation;
+import user.Guest;
+import employees.Receptionist;
 //Formater Classes
 import java.text.SimpleDateFormat;
-
 //Exeception Classes
 import java.lang.IndexOutOfBoundsException;
 import java.text.ParseException;
 import java.time.format.DateTimeParseException;
-import java.util.InputMismatchException;
 import java.io.IOException;
 import java.lang.ClassNotFoundException;
-import java.lang.IndexOutOfBoundsException;
-
+import javax.management.InvalidAttributeValueException;
+import java.lang.NullPointerException;
 //Collections Class
 import java.util.Collections;
-
 public class Reservation implements Comparable<Reservation>,Serializable{
-    //Static member variables
+    //Static array list which have all reservations
     private static ArrayList<Reservation> history=new ArrayList<>();
-    private static int idGenerator=0;
-
     //Object-related member variables 
     private int reservationNum;
     private int tableNum;
     private int receptionistId;
     private int guestId;
-    private ArrayList<String> order;
+    private ArrayList<Integer> order;
     private int numOfGuests;
     private double price;
     private byte rating;
     private Date date;
     private LocalTime startTime;
     private LocalTime endTime;
-    private int no_of_reservation=0;
-    private double revenue;
     //Constructor
     public Reservation(){
-       reservationNum=++idGenerator;
+       reservationNum=history.size()+1;
        date=new Date();
        order=new ArrayList<>();
        startTime=LocalTime.MIDNIGHT;
        endTime=LocalTime.MIDNIGHT;
        history.add(this);
-       no_of_reservation++;
     }
-    
     //Setters
-    public void setReceptionistId(int id){receptionistId=id;}
-    public void setGuestId(Scanner take){
-        boolean check=true;
-        while(check){
-            System.out.print("Guest ID: ");
-            guestId=take.nextInt();
-            System.out.print("\n");
-            Guest temp=Guest.getGuest(guestId);
-            if(temp!=null){
-                check=false;
-            }
-            else{
-                System.out.println("Guest not found!");
-            }
-            
+    public void setReceptionistId(int id)throws InvalidAttributeValueException{
+        Receptionist temp=Receptionist.search(id);
+        if(temp!=null){
+            receptionistId=id;
         }
-        
-    }
-    public void setTableNum(Scanner take){
-        boolean check=true;
-        while(check){
-            System.out.print("Table number: ");
-            tableNum=take.nextInt();
-            System.out.print("\n");
-            try{
-                Table.getTable(tableNum);
-                check=false;
-            }catch(IndexOutOfBoundsException e){
-                System.out.println("Table not found!");
-            }
+        else{
+            throw new InvalidAttributeValueException("Receptionist not found!");
         }
     }
-    public void setNumOfGuests(Scanner take){
-        boolean check=true;
-        while(check){
-            System.out.print("Number of Guests: ");
-            try{
-                numOfGuests=take.nextInt();
-                check=false;
-            }catch(InputMismatchException e){ 
-                System.out.println("Invalid input"); 
-                take.next();
-            }
-            System.out.print("\n");
+    public void setGuestId(int id)throws InvalidAttributeValueException{
+        Guest temp=Guest.getGuest(id);
+        if(temp!=null){
+            guestId=id;
+        }
+        else{
+            throw new InvalidAttributeValueException("Guest not found!");
         }
     }
-    public void setDate(Scanner take){
-        boolean check=true;
-        while(check){
-            System.out.print("Date (dd/mm/yyyy): ");
-            String input=take.next();
-            System.out.print("\n");
-            SimpleDateFormat sf=new SimpleDateFormat("dd/MM/yyyy");
-            try{
-                date=sf.parse(input);
-                check=false;
-            }catch(ParseException e){
-                System.out.println("----------Invalid Date input-------------");
-            }
+    public void setTableNum(int tableNumber) throws InvalidAttributeValueException{
+        try{
+            Table.getTable(tableNum);
+        }catch(IndexOutOfBoundsException e){
+            throw new InvalidAttributeValueException("Table not found!");
         }
     }
-    public void setStartTime(Scanner take){
-        boolean check=true;
-        while(check){
-            System.out.print("Start Time: ");
-            String input=take.next();
-            System.out.print("\n");
-            try{
-                startTime=LocalTime.parse(input);
-                check=false;
-            }catch(DateTimeParseException e){
-                System.out.println("----------Invalid Time input-------------");
-            }
+    public void setNumOfGuests(int num) throws InvalidAttributeValueException{
+        Table reserved=Table.getTable(tableNum);
+        if(num>reserved.getNoOfSeats()){
+            throw new InvalidAttributeValueException("Guests can't be more than "+reserved.getNoOfSeats());
         }
     }
-    public void setEndTime(Scanner take){
-        boolean check=true;
-        while(check){
-            System.out.print("End Time: ");
-            String input=take.next();
-            System.out.print("\n");
-            try{
-                endTime=LocalTime.parse(input);
-                check=false;
-            }catch(DateTimeParseException e){
-                System.out.println("----------Invalid Time input-------------");
-            }
+    public void setDate(String input) throws ParseException{
+        SimpleDateFormat sf=new SimpleDateFormat("dd/MM/yyyy");
+        try{
+            date=sf.parse(input);
+        }catch(ParseException e){
+            throw e;
         }
     }
-    public void setRating(byte guestRating) throws IndexOutOfBoundsException{
+    public void setStartTime(String input) throws DateTimeParseException{
+        try{
+            startTime=LocalTime.parse(input);
+        }catch(DateTimeParseException e){
+            throw e;
+        }  
+    }
+    public void setEndTime(String input) throws DateTimeParseException,InvalidAttributeValueException{
+        try{
+            endTime=LocalTime.parse(input);    
+        }catch(DateTimeParseException e){
+            throw e;
+        }
+        if(endTime.compareTo(startTime)<0){
+            throw new InvalidAttributeValueException("----------Invalid Time input-------------");
+        }else if(isReserved()){
+            throw new InvalidAttributeValueException("Table is reserved in the specified time.");
+        }  
+    }
+    public void setRating(byte guestRating) throws InvalidAttributeValueException{
         if(guestRating>=0 && guestRating<=10){
             rating=guestRating;
         }
         else{
-            throw new IndexOutOfBoundsException("Rating must be (0->10)");
+            throw new InvalidAttributeValueException("Rating must be (0->10)");
         }
     }
-    
     //Functions
-    public void takeOrder(Scanner get){
-        boolean check=true;
-        int menuId=-1;
-        ArrayList<Menu> menues=Menu.getlist();
-        for(int i=0;i<menues.size();i++){
-            System.out.println(""+(i+1)+menues.get(i).Categ.toString());
-        }
-        System.out.println("Enter Number of the menu you want:");
-        while(check){
-            try{
-                menuId=get.nextInt();
-                check=false;
-            }catch(InputMismatchException e){
-                System.out.println("Invalid input(integer is required)");
-                get.next();
+    public void takeOrder(ArrayList<Meal> orderOfMeals) throws NullPointerException{
+        for(int i=0;i<orderOfMeals.size();i++){
+            if(orderOfMeals.get(i)!=null){
+                order.add(orderOfMeals.get(i).getMeal_ID());
             }
-            if(menuId<0||menuId>menues.size()){
-                System.out.println("Invalid input");
-                check=true;
+            else{
+                throw new NullPointerException("Meal pointer can't be equal null");
             }
-        }
-        check=true;
-        Menu current=menues.get(menuId);
-        System.out.println("Menu ELements:");
-        for(int i=0;i<current.Meals.size();i++){
-            System.out.println(""+(i+1)+current.Meals.get(i).getName());
-        }
-        System.out.println("Enter Number of the meal you want to add(Any other number to exit):");
-        int i=1;
-        while(i>0&&i<=current.Meals.size()){
-            while(check){
-                try{
-                    i=get.nextInt();
-                    check=false;
-                }catch(InputMismatchException e){
-                    System.out.println("Invalid input(integer is required)");
-                    get.next();
-                }
-            }
-            check=true;
-            order.add(current.Meals.get(i-1).getName());
-            System.out.println("item is added successfully");
         }
         calculatePayment();
     }
     private void calculatePayment(){
         price+=Table.getTable(tableNum).getCost();
         for(int i=0;i<order.size();i++){
-            //price+=order.get(i).getPrice();
+            price+=Meal.getMealById(order.get(i)).getPrice();
         }
         price*=1.14; //taxes
     }
-    
-    public boolean isReserved(){
+    private boolean isReserved(){
         return Table.getTable(tableNum).isReserved(date, startTime, endTime);
     }
-    public boolean checkNoOfGuests(){
-        return(numOfGuests<=Table.getTable(tableNum).getNoOfSeats());
-    }
-
     //Getters
     public int getReservationNumber(){return reservationNum;}
     public Date getDate(){return date;}
     public LocalTime getStartTime(){return startTime;}
     public LocalTime getEndTime(){return endTime;}
     public double getPrice(){return price;}
-
+    public  static ArrayList<Reservation> getList() { return history; }
+    public int getTableNum() { return tableNum; }
+    public int getReceptionistId() { return receptionistId; }
+    public int getGuestId() { return guestId; }
+    public int getNumOfGuests() { return numOfGuests;}
     //static search functions
     public static Reservation search(int id){
         Collections.sort(history);
@@ -249,8 +167,8 @@ public class Reservation implements Comparable<Reservation>,Serializable{
     public static ArrayList<Reservation>search(Table key){
         ArrayList<Reservation> result=new ArrayList<>();
         for(int i=0;i<history.size();i++){
-            //if(history.get(i).tableNum==key.getTableId())
-            //result.add(history.get(i));
+            if(history.get(i).tableNum==key.getTableID())
+            result.add(history.get(i));
         }
         return result;
     }
@@ -270,15 +188,24 @@ public class Reservation implements Comparable<Reservation>,Serializable{
         }
         return result;
     }
+    public static ArrayList<Reservation>search(Meal key){
+    ArrayList<Reservation> result=new ArrayList<>();
+    
+    for(int i=0;i<history.size();i++){
+        for(int j=0;j<history.get(i).order.size();j++){
+            if(history.get(i).order.get(j)==key.getMeal_ID()){
+                result.add(history.get(i));
+            }
+        }
+    }
+     return result;
+    }
     //Reading & Writing in binary files methods
     public static void getRecord(){
         try{
             FileInputStream f=new FileInputStream("Reservations archive.dat");
             ObjectInputStream in=new ObjectInputStream(f);
-            int size=in.readInt();
-            for(int i=0;i<size;i++){
-                history.add((Reservation)in.readObject());
-            }    
+            history=(ArrayList<Reservation>)in.readObject(); 
             in.close();
             f.close();
         }catch(IOException e){
@@ -291,17 +218,13 @@ public class Reservation implements Comparable<Reservation>,Serializable{
         try{
             FileOutputStream f=new FileOutputStream("Reservations archive.dat");
             ObjectOutputStream out=new ObjectOutputStream(f);
-            out.writeInt(history.size());
-            for(int i=0;i<history.size();i++){
-                out.writeObject(history.get(i));
-            }    
+            out.writeObject(history);   
             out.close();
             f.close();
         }catch(IOException e){
             System.out.println("Error happened writing in the file: Reservation archive");
         }
     }
-
     //Overriden object functions
     @Override
     public String toString(){
@@ -315,37 +238,4 @@ public class Reservation implements Comparable<Reservation>,Serializable{
         else if(reservationNum<right.reservationNum)return -1;
         else return 0;
     }
-     public  static ArrayList getList() {
-       return history;
-   }
-
-    public static int getIdGenerator() {
-        return idGenerator;
-    }
-
-    public int getTableNum() {
-        return tableNum;
-    }
-
-    public int getReceptionistId() {
-        return receptionistId;
-    }
-
-    public int getGuestId() {
-        return guestId;
-    }
-
-    public int getNumOfGuests() {
-        return numOfGuests;
-    }
-
-    public int getNo_of_reservation() {
-        return no_of_reservation;
-    }
-
-    public double getRevenue() {
-        return revenue;
-    }
-     
-     
 }
