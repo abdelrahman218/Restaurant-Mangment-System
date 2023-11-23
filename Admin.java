3c67cc455398eaba82420488a8f6d0fab278be9a
Ryderclaw <120842081+Ryderclaw@users.noreply.github.com>
2023-11-23 13:07:11 +0200

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,61 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package javaapplication3;
 import java.util.ArrayList;
 import java.util.Date;
+import java.time.LocalTime;
 import java.util.Arrays;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+import javax.management.InvalidAttributeValueException;
+public class Admin extends PersonClass  {
 
-public class Admin  {
-private String user= "";
-private String password="";
 
-
-public Admin(String username, String pass){
-    user= username;
-    password =pass;
-}
-public String getUsername(){
-
-return user;
-}
-public void setUsername(String username){
-user=username;
+public Admin(String Name, String Address, String DateOfBirth, String PhoneNum, String Email,String UserName,String Password){
+    super(Name,Address,DateOfBirth,PhoneNum,Email,UserName, Password);
 }
 
-public void setPassword(String pass){
-password=pass;
-}
-public boolean checkPassword(String pass){
-
-return (pass==password);
-}
 public void addTable(int numofseats){
 
  Table table=new Table(numofseats);
- ArrayList<Table>Tables=Table.getList();
+ ArrayList<Table>Tables=Table.getlist();
  Tables.add(table);
 }
 public void editTable(int tableNum,Category tableCategory,double newcost,int newnoseats){
-    ArrayList<Table> Tables=Table.getList();
-    Tables.get(tableNum).Categ=tableCategory;
-    Tables.get(tableNum).Cost=newcost;
-    Tables.get(tableNum).NoOfSeats=newnoseats;
+    ArrayList<Table> Tables=Table.getlist();
+    Tables.get(tableNum).setCateg(tableCategory);
+    Tables.get(tableNum).setCost(newcost);
+    Tables.get(tableNum).setNoOfSeats(newnoseats);
 }
 public void removeTable(int tableNum) throws Throwable{
-    ArrayList<Table> Tables=Table.getList();
-    Tables.get(tableNum).finalize();
+    ArrayList<Table> Tables=Table.getlist();
+    Table t=Tables.get(tableNum);
+    Tables.remove(t);
     
 }
 
-public String search(Category tableCategory){
-    ArrayList<Table> Tables=Table.getList();
-    int count =0;
+public String searchTable(Category tableCategory){
+    ArrayList<Table> Tables=Table.getlist();
     int []no=new int[10];
     for(int i=0;i<Tables.size();i++){
-if(Tables.get(i).Categ==tableCategory){
-count++;
+if(Tables.get(i).getcateg()==tableCategory){
 no[i]=i;
 }
 else{
@@ -65,11 +48,14 @@ continue;
     return Arrays.toString(no);
 }
 
-public void viewTableReports( int tableNum){
-   ArrayList<Table> Tables=Table.getList();
-System.out.println("The table Category is "+ Tables.get(tableNum).Categ);
-System.out.println("The table most number of seats is "+Tables.get(tableNum).NoOfSeats);
-System.out.println("The table's cost"+Tables.get(tableNum).Cost);
+public void viewTableReports( int tableNum,Date StartDate,Date EndDate){
+   ArrayList<Table> Tables=Table.getlist();
+System.out.println("The table Category is "+ Tables.get(tableNum).getcateg());
+System.out.println("The number of times this table got reserved between time "+StartDate.toString()+" and "+EndDate.toString()+Tables.get(tableNum).ReservationsInSpecificTime(StartDate, EndDate));
+System.out.println("Table with the highest revenue is "+Table.highestRevenue(StartDate, EndDate));
+    System.out.println("Table that is most reserved is "+Table.mostReservedTable(StartDate, EndDate));    
+System.out.println("The table most number of seats is "+Tables.get(tableNum).getNoOfSeats());
+System.out.println("The table's cost"+Tables.get(tableNum).getCost());
 
 
 
@@ -90,24 +76,24 @@ Menues.get(menuId).Meals.set(i, tmeal);
     
     
 }
-public void removeMenu(int menuId){
+public void removeMenu(int menuId) throws Throwable{
    ArrayList<Menu>Menues=Menu.getlist();
    Menues.get(menuId).finalize();
 
 }
 
-public void search( int menuId,Date Start,Date End){
+public void searchMenu( int menuId,Date Start,Date End){
  ArrayList<Menu>Menues=Menu.getlist();
  System.out.println("This menu's category is"+Menues.get(menuId).Categ);
  System.out.println("This menu's most ordered meal is"+Menues.get(menuId).mostOrderedMeal(Start, End));
 }
-public void viewMenuReports(int menuId){
+public void viewMenuReports(Date Start,Date End){
 ArrayList<Menu>Menues=Menu.getlist();
-System.out.println("This menu belongs to Table : "+Menues.get(menuId).Categ);
-for(int i=0;i<Menues.get(menuId).Meals.size();i++){
-System.out.println("This menu belongs to Table : "+Menues.get(menuId).Meals.get(i));
+for(int i=0;i<Menues.size();i++){
+System.out.println("This menu belongs to Table : "+Menues.get(i).Categ);
+//    System.out.println("This menu most ordered meal is : "+Menues.get(i).mostOrderedMeal(Start, End)+" and it's category is : "Menues.get(i).);
+    
 }
-
 }
 public void addUsers(String name, String Address, String DateOfBirth, String PhoneNum, String Email){
 Guest guest=new Guest(name,Address,DateOfBirth,PhoneNum,Email);
@@ -115,7 +101,7 @@ ArrayList<Guest>guests=Guest.getList();
 guests.add(guest);
 
 }
-public void edit(int UserId,String Categorys){
+public void editCategory(int UserId,String Categorys){
 Guest Guests= Guest.getGuest(UserId);
 if(Categorys.equalsIgnoreCase("couples")){
 Guests.PreferredCategory=Category.Couples;
@@ -171,13 +157,13 @@ id=recep.get(i).getName();
     System.out.println("The receptionist whose name is "+id+"with the highest revenue is "+max);
 ArrayList<Reservation>h=Reservation.getList();
 for(int j=0;j<h.size();j++){
-    System.out.println("The Receptionist whose name is "+recep.get(j).getName()+"his reservation number is "+h.get(j).getNo_of_reservation());
+    System.out.println("The Receptionist whose name is "+recep.get(j).getName()+"his reservation number is "+h.get(j).getReservationNumber());
 }
-double maxreserve=recep.get(0).getReservationcount();
+double maxreserve=recep.get(0).getreservationsCount();
 String name =recep.get(0).getName();
 for(int j=0;j<h.size();j++){
-  if(recep.get(j).getReservationcount()>maxreserve){
-maxreserve=recep.get(j).getReservationcount();
+  if(recep.get(j).getreservationsCount()>maxreserve){
+maxreserve=recep.get(j).getreservationsCount();
 name=recep.get(j).getName();
 }   
 }
@@ -190,7 +176,7 @@ Guest guests =Guest.getGuest(GuestId);
 ArrayList<Reservation> G=r.search(guests);
     System.out.println("The guest whose id is :"+GuestId+" his reservation history is");
 for(int i=0;i<G.size();i++){
-    System.out.println("Reservation number "+i+1);
+    System.out.println("Reservation number "+G.get(i).getReservationNumber());
     System.out.println("whose date is "+G.get(i).getDate());
     System.out.println("whose receptionist is "+G.get(i).getReceptionistId());
     System.out.println("whose reservation number"+G.get(i).getReservationNumber());
@@ -199,19 +185,22 @@ for(int i=0;i<G.size();i++){
     System.out.println("whose start time is "+G.get(i).getStartTime());
     System.out.println("whose end time is "+G.get(i).getEndTime());
 }
-}
-
-void rating(byte rate,int guestid){
-
-ArrayList<Reservation> G=Reservation.getList();
-for(int i=0;0<G.size();i++){
-if(G.get(i).getGuestId()==guestid){
-G.get(i).setRating(rate);
-}
-else{
-continue;
-}
-}
+    System.out.print("Choose which reservation to get is cost : ");
+    Scanner s =new Scanner(System.in);
+    int check=s.nextInt();
+    for(int i=0;i<G.size();i++){
+    if(check==G.get(i).getReservationNumber()){
+        System.out.println("The reservation cost is : "+G.get(i).getPrice());
+    }
+    }
+    double sum=0;
+    double average;
+  for(int i=0;i<G.size();i++){
+  sum=G.get(i).getPrice();
+  
+  }
+  average=sum/G.size();
+    System.out.println("The average cost of all reservations is : "+average);
 }
 }
 
