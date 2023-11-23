321d279404b190f3d48399ad7b6a26e7c2d6ac36
Omar400-cpu <148485672+Omar400-cpu@users.noreply.github.com>
2023-11-23 11:56:47 +0200

    Update Meal.java

--- Meal.java
+++ Meal.java
@@ -1,9 +1,19 @@
 package System;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-public class Meal {
+import java.io.Serializable;
+import java.util.Scanner;
 
-   private static ArrayList<Meal> mealList = new ArrayList<>();
+public class Meal implements Serializable {
+
+    private static ArrayList<Meal> mealList = new ArrayList<>();
     private int meal_ID;
     private String Name;
     private double Price;
@@ -21,7 +31,7 @@ public class Meal {
     public int getMeal_ID() {
         return meal_ID;
     }
-   
+
     protected String getName() {
         return Name;
     }
@@ -49,10 +59,36 @@ public class Meal {
     public static Meal getMealById(int id) {
         for (int i = 0; i < mealList.size(); i++) {
             Meal meal = mealList.get(i);
-            if (meal.getMeal_ID()==id) {
+            if (meal.getMeal_ID() == id) {
                 return meal;
             }
         }
-        return null; 
+        return null;
+    }
+
+    public void ReadToFile() {
+        try {
+            FileInputStream i = new FileInputStream("Meal.dat");
+            ObjectInputStream o = new ObjectInputStream(i);
+            mealList = (ArrayList<Meal>) o.readObject();
+            i.close();
+            o.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }catch(ClassNotFoundException e){
+        System.out.println(e);
+        }
+    }
+
+    public void WriteInFile() {
+        try {
+            FileOutputStream i = new FileOutputStream("Meal.dat");
+            ObjectOutputStream o = new ObjectOutputStream(i);
+            o.writeObject(mealList);
+            o.close();
+            i.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
