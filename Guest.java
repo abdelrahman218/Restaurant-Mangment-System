d8cd1295e312cf823c273b0637f895531e1f726d
MostafaAyman-14 <149167922+MostafaAyman-14@users.noreply.github.com>
2023-11-26 11:47:46 -0800

    Add files via upload

--- Guest.java
+++ Guest.java
@@ -2,6 +2,7 @@ package user;
 import java.util.ArrayList;
 import employees.Person;
 import java.util.Scanner;
+import javax.management.InvalidAttributeValueException;
 import system.Category;
 import system.Reservation;
 public class Guest extends Person {
@@ -11,17 +12,11 @@ public class Guest extends Person {
         super(Name,Address,DateOfBirth,PhoneNum,Email,UserName,Password);
         Guests.add(this);
     }
-     public  void ViewReservation(){
-         int id=0;
-       for(int i=0;i<Guests.size();i++){
-            id=Guests.get(i).getId();
-       }
-       if(id==0){
-           System.out.println("No current Reservation available for this guest");
-       }
-       else if(id>0){
-        Reservation.search(id);
-       }
+     public  String ViewReservation(){
+    
+        String list = Reservation.search(this).toString();
+               return list;
+      
     }
     
     public void setPreferedCategory(int key){
@@ -62,36 +57,14 @@ public class Guest extends Person {
     public static ArrayList<Guest> getGuests() {
         return Guests;
     }
-    public static void GetCurrentGuest(){
-         for(int i=Guests.size()-1;i<Guests.size();i++){
-              String Name=Guests.get(i).getName();
-              String Address =Guests.get(i).getAddress();
-              String DateOfBirth =Guests.get(i).getDateOfBirth();
-              String Email=Guests.get(i).getEmail();
-              String Pass =Guests.get(i).getPassword();
-              String PhoneNum=Guests.get(i).getPhoneNum();
-              String UserName=Guests.get(i).getUserName();
-              int ID=Guests.get(i).getId();
-              Category ge=Guests.get(i).getPreferredCategory();
-       }
-    }
     public Category getPreferredCategory() {
         return PreferredCategory;
     }
-    public void RateBooking(){
-        for(int i=0;i<Guests.size();i++){
-        Reservation.search(Guests.get(i).getId());
-       }
-        int Rating;
-        Scanner s=new Scanner(System.in);
-        Rating=s.nextInt();
-        if(Rating>=1&&Rating<=5){
-            System.out.println("Thank you for your rating");
-        }else{
-            System.out.println("invalid Rating please provide a rating between 1 and 5");
-        }
-    }
+
+public void RateBooking(byte Rating) throws InvalidAttributeValueException{
+    Reservation.search(this).getLast().setRating(Rating);
     
+}    
     
             
 }
