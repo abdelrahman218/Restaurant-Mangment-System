f310641ac2a6f2e92b5324d57aedd4f2a7ca9d61
abdelrahman218 <144768458+abdelrahman218@users.noreply.github.com>
2024-01-05 16:14:49 +0200

    Add files via upload

--- Employees/Receptionist.java
+++ Employees/Receptionist.java
@@ -32,45 +32,47 @@ public class Receptionist extends Person implements Comparable<Receptionist>{
     //Reservation Related functions
     public void createReservation(int guestId,int tableNum,int numOfGuests,String date,String start,String end,ArrayList<Meal>order)
     throws InvalidAttributeValueException,ParseException{
-
-        Reservation r=new Reservation();
-        try{
-            r.setReceptionistId(getId());
-            r.setGuestId(guestId);
-            r.setTableNum(tableNum);
-            r.setNumOfGuests(numOfGuests);
-            r.setDate(date);
-            r.setStartTime(start);
-            r.setEndTime(end);
-            r.takeOrder(order);
-        }catch(InvalidAttributeValueException e){
-            throw e;
-        }catch(ParseException parse){
-            throw parse;
+        synchronized (Reservation.getList()) {
+            Reservation r = new Reservation();
+            try {
+                r.setReceptionistId(getId());
+                r.setGuestId(guestId);
+                r.setTableNum(tableNum);
+                r.setNumOfGuests(numOfGuests);
+                r.setDate(date);
+                r.setStartTime(start);
+                r.setEndTime(end);
+                r.takeOrder(order);
+            } catch (InvalidAttributeValueException e) {
+                throw e;
+            } catch (ParseException parse) {
+                throw parse;
+            }
+            reservationsCount++;
+            revenue += r.getPrice();
+            Guest.getGuest(guestId).incrementReservation();
+            r.setReservationNum();
+            Reservation.getList().add(r);
         }
-        reservationsCount++;
-        revenue+=r.getPrice();
-        Guest.getGuest(guestId).incrementReservation();
-        r.setReservationNum();
-        Reservation.getList().add(r);
     }
     public void cancelReservation(int resId) throws InvalidAttributeValueException{
-        ArrayList<Reservation> reservations=Reservation.search(this);
-        Reservation toBeDeleted=Reservation.search(resId);
-        for(int i=0;i<reservations.size();i++){
-            Reservation temp=reservations.get(i);
-            if(temp==toBeDeleted){
-                ArrayList<Integer> order=temp.getOrder();
-                for(int j=0;j<temp.getOrder().size();j++){
-                    Meal.getMealById(order.get(j).intValue()).decrementOrders();
+        synchronized (Reservation.getList()) {
+            ArrayList<Reservation> reservations = Reservation.search(this);
+            Reservation toBeDeleted = Reservation.search(resId);
+            for (Reservation temp:reservations) {
+                if (temp == toBeDeleted) {
+                    ArrayList<Integer> order = temp.getOrder();
+                    for (int j = 0; j < temp.getOrder().size(); j++) {
+                        Meal.getMealById(order.get(j).intValue()).decrementOrders();
+                    }
+                    reservationsCount--;
+                    Guest.getGuest(temp.getGuestId()).decrementReservation();
+                    Reservation.getList().remove(temp);
+                    return;
                 }
-                reservationsCount--;
-                Guest.getGuest(reservations.get(i).getGuestId()).decrementReservation();
-                reservations.remove(i);
-                return;
             }
+            throw new InvalidAttributeValueException("Reservation not found.");
         }
-        throw new InvalidAttributeValueException("Reservation not found.");
     }
     
     //Getter functions
@@ -125,6 +127,19 @@ public class Receptionist extends Person implements Comparable<Receptionist>{
         }catch(IOException e){
             System.out.println("Error happened writing in the file: Receptionist archive");
         }
+    }
+
+    //Login Function
+    public static Receptionist login(String username,String password) throws InvalidAttributeValueException {
+        for(Receptionist current : receptionists){
+            if(username.equals(current.getUserName())){
+                if(current.checkpassword(password)){
+                    return current;
+                }
+                throw new InvalidAttributeValueException("Password is incorrect");
+            }
+        }
+        throw new InvalidAttributeValueException("Username is incorrect");
     }
     
     //Overriden object functions
